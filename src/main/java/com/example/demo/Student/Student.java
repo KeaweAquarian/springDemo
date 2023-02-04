597b96f25d8d272a714f3ff1731c10334302ca16
KeaweAquarian <keaweaquarian@gmail.com>
2023-02-04 10:35:28 -0500

    updates

---
+++ src/main/java/com/example/demo/Student/Student.java
@@ -0,0 +1,11 @@
+package com.example.demo.Student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
+}
